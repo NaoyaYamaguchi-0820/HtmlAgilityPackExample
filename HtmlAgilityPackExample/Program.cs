6920c563c9d8bdce7de8c1106105b210b05ce8bb
NaoyaYamaguchi0820 <naoya.yamaguchi.mail@gmail.com>
2021-01-23 18:48:20 +0900

    プロジェクト ファイルを追加します。

---
+++ HtmlAgilityPackExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace wettest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient web = new WebClient();
+
+            // ここは保存したHTMLファイルのフルパスに書き換える
+            string url = @"C:\Users\naoya\Desktop\阿部寛のホームページ.html";
+            string htmlstr = web.DownloadString(url);
+
+            if (htmlstr != null)
+            {
+                var htmlDoc = new HtmlDocument();
+
+                htmlDoc.LoadHtml(htmlstr);
+                HtmlNode n = htmlDoc.DocumentNode.SelectSingleNode(@"/html/head/title");
+                Console.WriteLine(n.InnerText);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
